5323282fa49fa36a255f76b6e484b04a2be4a697
Yuri Cooliq <yurycooliq@gmail.com>
2026-06-04 03:31:54 +0100

    Added Dotenv IDE path for Prisma fixed

--- scripts/apply-migrations.cjs
+++ scripts/apply-migrations.cjs
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const { Client } = require('pg');
+require('dotenv').config({ path: path.join(__dirname, '..', '.env'), quiet: true });
 
 const databaseUrl = process.env.DATABASE_URL;
 
--- test/set-test-env.js
+++ test/set-test-env.js
@@ -1,1 +1,2 @@
+require('dotenv').config({ quiet: true });
 process.env.NODE_ENV = 'test';
